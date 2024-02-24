660e801bec01bf9d4637dc1b3ef3d1daff2c9491
Ojaswi-Srivastava <ojaswios1998@gmail.com>
2024-02-25 01:09:57 +0530

    added testscript for ECOMAUTO03

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('Product Selection and Verification',()=>{
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  test('has title', async ({ page }) => {
+    await page.goto('/');
+    await page.getByRole('link', { name: 'Store' }).click();
+    await expect(page.locator('.collection-hero__title')).toHaveText('Collection: Products')
+    await page.getByRole('link', { name: '5 Panel Camp Cap' }).click();
+    page.waitForURL('**\/5-panel-hat')
+    await expect(page.url()).toContain('5-panel-hat');
+    await expect(page.getByRole('heading',{ name: '5 Panel Camp Cap' })).toBeVisible()
+  });
+})
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
